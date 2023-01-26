363d434a005c351e7f970b2a339b131922872176
shiabulislam <md.shiabul.cse@gmail.com>
2023-01-26 22:43:26 +0600

    slider set new product

---
+++ frontend/src/components/index.js
@@ -0,0 +1,7 @@
+export {default as CartSidebar} from './CartSidebar.vue';
+export {default as FooterPart} from './FooterPart.vue';
+export {default as HeaderPart} from './HeaderPart.vue';
+export {default as LoginModal} from './LoginModal.vue';
+export {default as MobileMenu} from './MobileMenu.vue';
+export {default as NavBar} from './NavBar.vue';
+export {default as NewsLetter} from './NewsLetter.vue';
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import Index from "../views/pages/index.vue";
+import {UserLogin, UserRegister} from "@/views/auth/index.js";
 
 
 const routes = [
-    { path: '/', component: Index },
+    {path: '/', name: '/', component: Index, meta: { title: 'Home' }},
+    {path: '/auth/login', name: "user.login", component: UserLogin, meta: { title: 'User Login' }},
+    {path: '/auth/register', name: "user.register", component: UserRegister, meta: { title: 'User Register' }},
 
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.BASE_URL),
     routes,
 })
+
+const DEFAULT_TITLE = '404';
+router.beforeEach((to,) => {
+    document.title = to.meta.title || DEFAULT_TITLE;
+
+});
 export default router;
