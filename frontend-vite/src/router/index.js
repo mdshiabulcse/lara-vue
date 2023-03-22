4b60a45c1726218b5551d1f9f0b7f9da39a11e17
shiabulislam <md.shiabul.cse@gmail.com>
2023-03-23 01:43:01 +0600

    page 20 part dynamic page setup

--- frontend-vite/src/router/index.js
+++ frontend-vite/src/router/index.js
@@ -2,13 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Index from "../views/pages/index.vue";
 import {UserLogin, UserRegister} from "@/views/auth/index.js";
 const routes = [
-    { path: '/', name:'index', component: Index },
-    { path: '/auth/login', name:'user.login', component: UserLogin },
-    { path: '/auth/register', name:'user.register',component: UserRegister },
+    { path: '/', name:'index', component: Index ,meta:{title:'Home'}},
+    { path: '/auth/login', name:'user.login', component: UserLogin ,meta:{title:'User Login'}},
+    { path: '/auth/register', name:'user.register',component: UserRegister ,meta:{title:'User Register'}},
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes
+})
+const DEFAULT_TITLE =404;
+router.beforeEach((to, from, next) => {
+    document.title=to.meta.title || DEFAULT_TITLE;
+    next();
 })
 export default router;
